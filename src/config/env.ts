2752f13e724ef852d0307f0d83b6a319e7171404
Zyronon <Zyronon@163.com>
2025-10-27 00:30:41 +0800

    feat:further improve the word practice to solve the problem of too many reviews

--- src/config/env.ts
+++ src/config/env.ts
@@ -43,7 +43,7 @@ export const SoundFileOptions = [
 ]
 export const APP_VERSION = {
   key: 'type-words-app-version',
-  version: 1
+  version: 2
 }
 export const SAVE_DICT_KEY = {
   key: 'typing-word-dict',
